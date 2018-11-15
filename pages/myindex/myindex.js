e3648f5abbda1029e090e743ebb38de3206669e0
Fangluobin <q1217830193@163.com>
2018-11-15 15:19:36 +0800

    简单写了一下背单词时候的算法

--- pages/myindex/myindex.js
+++ pages/myindex/myindex.js
@@ -13,13 +13,27 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     user: {},
     //单词信息（伪）
-    word:{
-      name:"hello",
-      IPA:"英 [hə'ləʊ] 美 [həˈloʊ]",
-      means: "int.打招呼;哈喽，喂;你好，您好;表示问候\n\
-      n.“喂”的招呼声或问候声\nvi.喊“喂”",
-      uses: "Hello John, how are you?\n哈罗，约翰，你好吗？",
-    },
+    words:[
+      {
+        name:"hello",
+        IPA:"英 [hə'ləʊ] 美 [həˈloʊ]",
+        means: "int.打招呼;哈喽，喂;你好，您好;表示问候\n\
+        n.“喂”的招呼声或问候声\nvi.喊“喂”",
+        uses: "Hello John, how are you?\n哈罗，约翰，你好吗？",
+      },{
+        name: "bye",
+        IPA: "英 [baɪ] 美 [baɪ]",
+        means: "int.再见，回头见\nn.次要的东西; （体育比赛中）轮空",
+        uses: "Bye, see you tomorrow\n再见，明天见。",
+      }, {
+        name: "copy",
+        IPA: "英 [ˈkɒpi] 美 [ˈkɑ:pi] ",
+        means: "n.复制品; 一份; （报刊等的）稿件; 准备排印的书面材料\
+        \nvt.& vi.复制; 抄写; 容许复制的\nvt.复制; 模仿; 仿造…的样式或图案; 抄写",
+        uses: "I will send you a copy of the report.\n我会把这个报告的复印本寄给你。",
+      }
+    ],
+    word:{},
     //是否选择词库
     haslexicon:false,
     //是否点击显示答案
@@ -58,8 +72,25 @@ Page({
       console.log('你点了认识')
     } else {
       console.log('你点了不认识')
+      this.data.words.push(this.data.word);
     }
-    console.log(e)
+    var myshiftword = this.data.words.shift();
+    console.log(this.data.words);
+    if (typeof (myshiftword) != "undefined"){//如果没有背完所有单词
+      this.setData({
+        word: myshiftword,//把取出来的单词存入word
+        clicked: false,//遮盖答案
+        lastnum: this.data.words.length+1//修改剩余单词值
+      })
+    } else {//背完所有单词
+      this.setData({
+        word:{},//清空word
+        clicked: false,//遮盖
+        lastnum: 0,//清空剩余单词值
+        msg: "恭喜你，已经背完今天的所有单词！"//提示
+      })
+    }
+    // console.log(e)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -67,16 +98,15 @@ Page({
   onLoad: function (options) {
     console.log('你打开了小程序首页')
     //打开小程序
-    if (options.selected > 0) {
-      //如果有选择词库
+    if (options.selected > 0) {//如果有选择词库      
       console.log('获得的参数：' + options.selected)
       this.setData({
+        word: this.data.words.shift(),
         haslexicon: true
       })
     }
 
-    if(!this.data.haslexicon){
-      //如果未获取登录信息
+    if (!this.data.haslexicon) {//如果未获取登录信息   
       var content = this;
       if (app.globalData.userInfo) {
         this.setData({
@@ -105,6 +135,10 @@ Page({
         })
       }
       console.log(this.data.user)
+    } else {//如果获取登录信息
+      this.setData({//初始化信息
+        lastnum: this.data.words.length+1
+      })
     }
   },
   getUserInfo: function (e) {
